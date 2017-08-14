5808de0479d5de4c92bbf09e2f818dc2a2ab3065
Anqush Jamdagani <njamdagani@gmail.com>
2017-08-14 19:17:41 +0530

    some loopholes filled

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-	var scrollTop = window.pageYOffset; // get number of pixels document has scrolled vertically 
+$(document).ready(function()
+{
+	var scrollTop = window.pageYOffset; // get number of pixels document has scrolled vertically
+	$("#abc").on("click",function()
+	{
+		$("#sign-in").toggleClass("active");
+	}); 
 });
